2ee4d04bbd72cefccca76b6070ad60296163eb29
Matthieu Ha L3CDPN 2014 <matthieu.ha@gmail.com>
2015-01-14 12:01:53 +0100

    playing with templates

--- app/router.js
+++ app/router.js
@@ -6,6 +6,17 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route("addUser", {
+    path: "addUser"
+  });
+
+  this.route("displayAll", {
+    path: "display"
+  });
+
+  this.route("details", {
+    path: "details"
+  });
 });
 
 export default Router;
